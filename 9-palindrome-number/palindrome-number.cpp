62aa03c1b2b097df3c2c9c2339b93d2b4cc2f315
naharnomika-jpg <naharnomika@gmail.com>
2026-09-15 17:17:26 +0530

    Time: 1 ms (57.11%) | Memory: 11 MB (7.09%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){return false;}
+        string str=to_string(x);
+        int left=0;
+        int right=str.length()-1;
+        while(left<right){
+            if(str[left]!=str[right]){
+                return false;
+            }
+            right --;
+            left ++;
+        }
+        return true;
+    }
+};
